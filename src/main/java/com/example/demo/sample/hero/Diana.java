0599a84154039262a6ff7f2901d78fa655b1f6d4
zhaoyang <980355088@qq.com>
2022-03-25 16:49:04 +0800

    update

---
+++ src/main/java/com/example/demo/sample/hero/Diana.java
@@ -0,0 +1,44 @@
+/**
+ * @作者 7七月
+ * @微信公号 林间有风
+ * @开源项目 $ http://7yue.pro
+ * @免费专栏 $ http://course.7yue.pro
+ * @我的课程 $ http://imooc.com/t/4294850
+ * @创建时间 2019-12-26 10:33
+ */
+package com.example.demo.sample.hero;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Diana {
+//    private String skillName = "Diana R";
+//    private String name;
+//    private Integer age;
+
+//    public Diana(String name, Integer age){
+//        this.name = name;
+//        this.age = age;
+//    }
+
+//    public Diana() {
+//        System.out.println("Hello,7yue");
+//    }
+
+    public void q(){
+        System.out.println("Diana Q");
+    }
+
+    public void w(){
+        System.out.println("Diana W");
+    }
+
+    public void e(){
+        System.out.println("Diana E");
+    }
+
+    public void r(){
+        System.out.println("Diana R");
+    }
+}
