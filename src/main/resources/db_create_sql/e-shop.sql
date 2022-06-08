6eb3d18badc9f430a34ced8ded487160e771d4a7
zhaoyang <zhaoyang@lyky.cn>
2022-06-08 15:48:30 +0800

    update

--- src/main/resources/db_create_sql/e-shop.sql
+++ src/main/resources/db_create_sql/e-shop.sql
@@ -16,25 +16,25 @@ Date: 2022-03-26 23:06:36
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for shop-cart
+-- Table structure for shop_cart
 -- ----------------------------
-DROP TABLE IF EXISTS `shop-cart`;
-CREATE TABLE `shop-cart` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `shop_cart`;
+CREATE TABLE `shop_cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `product_id` int DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   `quantity` int DEFAULT NULL,
-  `create_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
--- Table structure for shop-order
+-- Table structure for shop_order
 -- ----------------------------
-DROP TABLE IF EXISTS `shop-order`;
-CREATE TABLE `shop-order` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `shop_order`;
+CREATE TABLE `shop_order` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `order_no` varchar(128) DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   `total_price` int DEFAULT NULL,
@@ -44,20 +44,20 @@ CREATE TABLE `shop-order` (
   `order_status` int DEFAULT NULL,
   `postage` int DEFAULT NULL,
   `payment_type` int DEFAULT NULL,
-  `pay_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `delivery_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `end_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `create_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `pay_time` timestamp NULL DEFAULT NULL,
+  `delivery_time` timestamp NULL DEFAULT NULL,
+  `end_time` timestamp NULL DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
--- Table structure for shop-order-item
+-- Table structure for shop_order-item
 -- ----------------------------
-DROP TABLE IF EXISTS `shop-order-item`;
-CREATE TABLE `shop-order-item` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `shop_order-item`;
+CREATE TABLE `shop_order-item` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `order_no` varchar(128) DEFAULT NULL,
   `product_id` int DEFAULT NULL,
   `product_name` varchar(100) DEFAULT NULL,
@@ -65,17 +65,17 @@ CREATE TABLE `shop-order-item` (
   `unit_price` int DEFAULT NULL,
   `quantity` int DEFAULT NULL,
   `total_price` int DEFAULT NULL,
-  `create_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
--- Table structure for shop-product
+-- Table structure for shop_product
 -- ----------------------------
-DROP TABLE IF EXISTS `shop-product`;
-CREATE TABLE `shop-product` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `shop_product`;
+CREATE TABLE `shop_product` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `image` varchar(500) DEFAULT NULL,
   `detail` varchar(500) DEFAULT NULL,
@@ -83,36 +83,39 @@ CREATE TABLE `shop-product` (
   `price` int DEFAULT NULL,
   `stock` int DEFAULT NULL,
   `status` int DEFAULT NULL,
-  `create_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
--- Table structure for shop-user
+-- Table structure for shop_coupon
 -- ----------------------------
-DROP TABLE IF EXISTS `shop-user`;
-CREATE TABLE `shop-user` (
+DROP TABLE IF EXISTS `shop_coupon`;
+CREATE TABLE `shop_product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `detail` varchar(500) DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- ----------------------------
+-- Table structure for shop_user
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_user`;
+CREATE TABLE `shop_user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(32) DEFAULT NULL COMMENT '用户名',
   `password` varchar(50) DEFAULT NULL,
   `password_salt` varchar(50) DEFAULT NULL,
   `mobile` varchar(15) DEFAULT NULL,
   `role` int(4) unsigned zerofill DEFAULT NULL,
-  `create_time` timestamp NULL DEFAULT NULL CURRENT_TIMESTAMP,
-  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='用户名';
-
--- ----------------------------
--- Table structure for usertable
--- ----------------------------
--- DROP TABLE IF EXISTS `usertable`;
--- CREATE TABLE `usertable` (
---   `username` varchar(255) DEFAULT NULL,
---   `userpassword` varchar(255) DEFAULT NULL,
---   `age` int DEFAULT NULL,
---   `sex` varchar(255) DEFAULT NULL,
---   `user_id` int NOT NULL AUTO_INCREMENT,
---   PRIMARY KEY (`user_id`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
