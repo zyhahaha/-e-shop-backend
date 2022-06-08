d49ff649de3c8d60443ddd96347b27eee2d1f380
zhaoyang <zhaoyang@lyky.cn>
2022-06-08 15:03:44 +0800

    添加swagger功能

--- src/main/java/com/example/demo/api/v1/user/UserController.java
+++ src/main/java/com/example/demo/api/v1/user/UserController.java
@@ -2,6 +2,9 @@ package com.example.demo.api.v1.user;
 
 import com.example.demo.api.v1.user.model.User;
 import com.example.demo.api.v1.user.serverimpl.UserServerimpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/user")
+@Api(tags = "用户控制器")
 public class UserController {
 
     @Autowired
     private UserServerimpl userServer;
 
+    @ApiOperation(value = "获取所有用户", notes = "查询分页数据")
+    @ApiImplicitParam(name = "name", value = "姓名")
     @RequestMapping("/getuserlist")
     public List<User> getUserList()
     {
